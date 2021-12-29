3a8c802cdf711370d9aca667469ac07694839a66
Ryokozai <78350717+Ryokozai@users.noreply.github.com>
2021-12-28 23:23:50 -0500

    Delete scripts directory

--- scripts/app.js
+++
@@ -1,8 +0,0 @@
-//scripts/app.js-welcome
-const menuToggle = document.querySelector('.toggle')
-const box = document.querySelector('.box')
-
-menuToggle.addEventListener('click', () => {
-	menuToggle.classList.toggle('active')
-	box.classList.toggle('active')
-})
